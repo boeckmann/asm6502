69bea66fbe1eeed419dfd1f59219d72945f54958
Bernd Boeckmann <bernd-git@boeckmann.io>
2022-10-09 22:57:35 +0200

    Modified test02.

--- tests/test02.asm
+++ tests/test02.asm
@@ -6,15 +6,16 @@ CHROUT = $FFD2
 ; BASIC code: 1024 SYS 2062
 .byte $0C,$08,$40,$00,$9E,$20,$32,$30,$36,$32,$00,$00,$00 
 
-  jsr CLRSCR    ; Call the Function that clears the screen
-  ldx #$00      ; Put 0 in Register X (Index Register)
+  jsr CLRSCR
+  ldx #$00
  
 write:
-  lda hello,x   ; Read next character from Address at Label .hello + Offeset X
-  jsr CHROUT    ; CHROUT Subroutine, prints the Character loaded into Register A
-  inx           ; Increments Register X by 1
-  cpx #$0B      ; Compare if Value in Register X equals to 11
-  bne write     ; If Value in Register X is not 11, go back to $033E
-  rts           ; Return to Basic
+  lda hello,x
+  jsr CHROUT
+  inx
+  cpx #$05
+  bne write
+  rts
 
 hello:
+  .byte 72,69,76,76,79
