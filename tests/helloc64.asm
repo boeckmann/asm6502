aac9502397cceef543a9212992f6f3ebe9db3d07
Bernd Boeckmann <bernd-codeberg@boeckmann.io>
2023-04-09 14:27:33 +0200

    adapt examples and tests to new features

--- tests/helloc64.asm
+++ tests/helloc64.asm
@@ -1,21 +1,23 @@
 ; C64 Hello World
-; assemble to .PRG file: asm6502 helloc64.asm helloc64.prg
+; assemble to .PRG file: asm6502 helloc64.asm helloc64.prg helloc64.lst
 
-.include "c64prg.inc"   ; .PRG header and BASIC upstart
-.include "c64krnl.inc"  ; KERNAL function addresses
+.include "c64prg.inc"           ; .PRG header and BASIC upstart
+.include "c64krnl.inc"          ; C64 kernal definitions
 
-start:
-        ldx #0
-        sta $d020
-@1      lda hello_msg,x
-        jsr CHROUT
-        inx
-        cpx #hello_len
-        bne @1
+; start: label gets implicitly defined by c64prg.inc, see listing
+        ldx #0                  ; load value 0 into X, beginning of string
+ @1     lda hello@msg,x         ; read string byte indexed by X
+                                ; hello@msg is a local label
+        jsr CHROUT              ; output char via kernal routine CHROUT
+        inx                     ; next string byte
+        cpx #hello@len          ; compare X with the string length stored in
+                                ; local variable hello@len
+        bne @1                  ; and jump to @1 if X < hello@len
         rts
 
-CR = $0C
-LF = $0A
+hello:                          ; hello string label with locals @msg and @len
+  @msg .byte "HELLO, WORLD!", CR, LF
+  @len = @ - @msg               ; calculate length of string
 
-hello_msg .byte "HELLO, WORLD!", CR, LF
-hello_len = @ - hello_msg
+CR = $0C                        ; give carrige return a symbolic name
+LF = $0A                        ; ... also line feed
